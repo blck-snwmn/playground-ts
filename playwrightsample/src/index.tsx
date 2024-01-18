251fa0c434e30d7a46a6269cd2d23801c81824a1
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-19 00:52:15 +0900

    Fix format

--- playwrightsample/src/index.tsx
+++ playwrightsample/src/index.tsx
@@ -1,44 +1,40 @@
-import type { FC } from 'hono/jsx'
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import type { FC } from "hono/jsx";
 
-const app = new Hono()
+const app = new Hono();
 
 const Layout: FC = (props) => {
-  return (
-    <html lang='en'>
-      <body>{props.children}</body>
-    </html>
-  )
-}
+	return (
+		<html lang="en">
+			<body>{props.children}</body>
+		</html>
+	);
+};
 
 const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
-  return (
-    <Layout>
-      <h1>Hello Hono!</h1>
-      <p>Here are the messages.</p>
-      <ul>
-        {props.messages.map((message) => {
-          return <li>{message}!!</li>
-        })}
-      </ul>
-    </Layout>
-  )
-}
+	return (
+		<Layout>
+			<h1>Hello Hono!</h1>
+			<p>Here are the messages.</p>
+			<ul>
+				{props.messages.map((message) => {
+					return <li>{message}!!</li>;
+				})}
+			</ul>
+		</Layout>
+	);
+};
 
-app.get('/', (c) => {
-  const messages = [
-    "hello world",
-    "hello hono",
-    "hello jsx",
-  ]
-  return c.html(<Top messages={messages} />)
-})
+app.get("/", (c) => {
+	const messages = ["hello world", "hello hono", "hello jsx"];
+	return c.html(<Top messages={messages} />);
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+	fetch: app.fetch,
+	port,
+});
