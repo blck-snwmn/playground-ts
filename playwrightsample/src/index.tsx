3b749c10345a62940c7ba8bf684fa7dc84a74f5d
blck-snwmn <whitesnowmancreator@gmail.com>
2024-01-19 00:51:00 +0900

    Add new sample for playwright

---
+++ playwrightsample/src/index.tsx
@@ -0,0 +1,44 @@
+import type { FC } from 'hono/jsx'
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+const Layout: FC = (props) => {
+  return (
+    <html lang='en'>
+      <body>{props.children}</body>
+    </html>
+  )
+}
+
+const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
+  return (
+    <Layout>
+      <h1>Hello Hono!</h1>
+      <p>Here are the messages.</p>
+      <ul>
+        {props.messages.map((message) => {
+          return <li>{message}!!</li>
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+app.get('/', (c) => {
+  const messages = [
+    "hello world",
+    "hello hono",
+    "hello jsx",
+  ]
+  return c.html(<Top messages={messages} />)
+})
+
+const port = 3000
+console.log(`Server is running on port ${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
