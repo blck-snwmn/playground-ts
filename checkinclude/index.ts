db100eda7c9639b9c68c0ed0d01d148a53202bd5
blck-snwmn <whitesnowmancreator@gmail.com>
2024-04-04 01:01:29 +0900

    Add sample

---
+++ checkinclude/index.ts
@@ -0,0 +1,10 @@
+const input = "testisblack";
+const check = "is"
+const index = input.indexOf(check);
+console.log(index);
+
+const before = input[index - 1];
+console.log(before);
+
+const after = input[index + check.length];
+console.log(after);
