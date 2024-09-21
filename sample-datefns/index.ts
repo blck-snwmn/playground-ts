aa755d4e746abf51797cc4e1fa84160b94b89449
blck-snwmn <whitesnowmancreator@gmail.com>
2024-09-21 23:29:52 +0900

    Add date-fns/tz sample

---
+++ sample-datefns/index.ts
@@ -0,0 +1,15 @@
+import { tz, TZDate } from "@date-fns/tz";
+import { addHours, format } from "date-fns";
+
+const nowD = new Date();
+const nowD2 = addHours(nowD, 1);
+console.log(nowD2.toString());
+
+
+const nowTZ = new TZDate(2024, 12, 1, "Asia/Singapore");
+const nowTZ2 = addHours(nowTZ, 1);
+console.log(nowTZ2.toString());
+
+const nowTZJst = new TZDate(2024, 12, 1, "JST");
+const nowTZJst2 = addHours(nowTZJst, 1);
+console.log(nowTZJst2.toString());
